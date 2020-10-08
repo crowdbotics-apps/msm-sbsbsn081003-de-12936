e379052fad05b82faf8d611446f905a61fa24360
Crowdbotics <team@crowdbotics.com>
2020-10-08 09:10:28 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,14 +1,7 @@
 import axios from "axios"
-import {
-  DSSLACKCONNECTOR1_USERNAME,
-  DSSLACKCONNECTOR1_PASSWORD
-} from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const dSSlackConnector = axios.create({
   baseURL: "https://api.spotify.com",
-  auth: {
-    username: DSSLACKCONNECTOR1_USERNAME,
-    password: DSSLACKCONNECTOR1_PASSWORD
-  },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 const msmSbsbsnAPI = axios.create({
